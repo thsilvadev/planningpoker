c707509607f66513178d18de24f26d9169ead44e
thsilvadev <thsilva.developer@gmail.com>
2025-07-20 07:22:56 -0400

    Eu gostaria de desmembrar melhor esses commits, mas infelizmente no meio do Go Horse acabou indo muita mudança junto. Agora o backend está bem mais robusto e à prova de erros/bugs. Temos reconexão, temos extensiva validação de acordo com a regra de negócio, temos mais eficiência e menos redundância, temos sair da sala/remover a sala entre outras coisas. Algumas coisas vão ficar de backlog mas esse backend está mais que pronto para um MVP.

--- backend/src/planning-poker/app/room/room.service.ts
+++ backend/src/planning-poker/app/room/room.service.ts
@@ -8,15 +8,16 @@ export class RoomService {
   //No início do serviço, inicializar um mapa para armazenar as salas
   private rooms: Map<string, Room> = new Map();
 
-  createRoom(moderatorName: string): Room {
+  createRoom(moderatorName: string, clientId: string): Room {
     // Criar uma nova sala com um ID único e o nome do moderador
     const newRoom: Room = {
       id: uuid(),
       creator: moderatorName,
+      creatorId: clientId,
       participants: [],
       tasks: [],
-      votesRevealed: false,
-      currentTaskId: undefined,
+      votingStatus: { status: 'idle' }, // Estado inicial da votação
+      updatedAt: new Date(), // Data de criação da sala
     };
 
     // Adicionar a nova sala ao mapa de salas
@@ -25,20 +26,77 @@ export class RoomService {
   }
 
   getRoom(roomId: string): Room | undefined {
-    return this.rooms.get(roomId);
+    const currentRoom = this.rooms.get(roomId);
+    if (currentRoom) {
+      //Atualiza a atividade da sala => para mensurar inatividade => para limpar salas inativas automaticamente.
+      currentRoom.updatedAt = new Date();
+    }
+    return currentRoom;
   }
 
-  addParticipant(roomId: string, participantName: string): Room | undefined {
+  addParticipant(
+    roomId: string,
+    participantName: string,
+  ): { newRoomState: Room; participantId: string } | undefined {
     const room = this.getRoom(roomId);
     if (room) {
       const participant: Participant = {
         id: uuid(),
         name: participantName,
-        vote: undefined,
         hasVoted: false,
       };
       room.participants.push(participant);
+      return { newRoomState: room, participantId: participant.id };
+    }
+  }
+
+  updateRoomActivity(roomId: string): void {
+    const room = this.getRoom(roomId);
+    if (room) {
+      room.updatedAt = new Date(); // Atualiza a data de atualização da sala
+    }
+  }
+
+  // Método para acessar todas as salas (necessário para o cleanup)
+  getAllRooms(): Map<string, Room> {
+    return this.rooms;
+  }
+
+  // Método para remover uma sala específica
+  removeRoom(roomId: string): boolean {
+    return this.rooms.delete(roomId);
+  }
+
+  getRoomByParticipantId(participantId: string | string[]): Room | undefined {
+    for (const room of this.rooms.values()) {
+      if (
+        room.participants.some(
+          (participant) => participant.id === participantId,
+        )
+      ) {
+        return room;
+      }
+    }
+    return undefined;
+  }
+
+  getRoomByCreatorId(creatorId: string | string[]): Room | undefined {
+    for (const room of this.rooms.values()) {
+      if (room.creatorId === creatorId) {
+        return room;
+      }
+    }
+    return undefined;
+  }
+
+  removeParticipant(roomId: string, participantId: string): Room | undefined {
+    const room = this.getRoom(roomId);
+    if (room) {
+      room.participants = room.participants.filter(
+        (participant) => participant.id !== participantId,
+      );
       return room;
     }
+    return undefined;
   }
 }
